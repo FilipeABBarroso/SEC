254532a92c3678a3a50c0f55aafe8094d20a4744
FilipeABBarroso <filipebarroso2812@gmail.com>
2022-03-30 11:06:42 +0100

    Add Exceptions

---
+++ Proto/src/main/java/tecnico/sec/proto/exceptions/NonceExceptions.java
@@ -0,0 +1,13 @@
+package tecnico.sec.proto.exceptions;
+
+import io.grpc.Status;
+
+public abstract class NonceExceptions extends BaseException{
+
+    public static class NonceNotFoundException extends NonceExceptions {
+        @Override
+        public Exception toResponseException() {
+            return Status.NOT_FOUND.asException();
+        }
+    }
+}
