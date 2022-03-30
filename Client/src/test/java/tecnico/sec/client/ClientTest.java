3dbe6731344349419b2b42767df76c75f4e71919
bernacv <bernardocvarzea@gmail.com>
2022-03-30 11:33:20 +0100

    Update ClientTest.java

--- Client/src/test/java/tecnico/sec/client/ClientTest.java
+++ Client/src/test/java/tecnico/sec/client/ClientTest.java
@@ -1,18 +1,41 @@
 package tecnico.sec.client;
 
+import org.junit.jupiter.api.Test;
+import tecnico.sec.KeyStore.singletons.KeyStore;
+import tecnico.sec.proto.exceptions.KeyExceptions;
+
+import java.security.KeyPair;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
 
-    @org.junit.jupiter.api.Test
-    void saveKeyPair() {
+    private final KeyPair keys;
+
+    ClientTest() throws KeyExceptions.NoSuchAlgorithmException {
+        keys = KeyStore.getCredentials();
     }
 
-    @org.junit.jupiter.api.Test
-    void loadKeyPair() {
+    @Test
+    void open_account() {
+        assertTrue(Client.open_account(keys.getPublic()));
     }
 
-    @org.junit.jupiter.api.Test
-    void generateCredentials() {
+    @Test
+    void send_amount() {
+
     }
+
+    @Test
+    void receive_amount() {
+    }
+
+    @Test
+    void check_account() {
+    }
+
+    @Test
+    void audit() {
+    }
+
 }
